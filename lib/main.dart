ac2a6c83397671cf8bff780cf0143adb12f0e97e
Muhammad rico <ricoexse@gmail.com>
2023-11-29 22:52:14 +0700

    splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fic7_app/pages/splash/splash_page.dart';
 
+import 'utils/light_themes.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,9 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: light,
       home: const SplashPage(),
     );
   }
--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
+import '../../utils/color_resources.dart';
+import '../../utils/images.dart';
+import 'widget/splash_painter.dart';
+
+
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+  const SplashPage({Key? key}) : super(key: key);
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
+  SplashScreenState createState() => SplashScreenState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class SplashScreenState extends State<SplashPage> {
+  final GlobalKey<ScaffoldMessengerState> _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        key: _globalKey,
+        body: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: ColorResources.getPrimary(context),
+              child: CustomPaint(
+                painter: SplashPainter(),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    Images.splashLogo,
+                    height: 250.0,
+                    fit: BoxFit.scaleDown,
+                    width: 250.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
