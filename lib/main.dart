c6f7bec3fd72ecebf85e05b2371d17b0a260b6dc
Muhammad rico <ricoexse@gmail.com>
2023-12-27 00:33:18 +0700

    register login logout fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_fic7_app/bloc/Categories/categories_bloc.dart';
-import 'package:flutter_fic7_app/bloc/Checkout/checkout_bloc.dart';
-import 'package:flutter_fic7_app/bloc/Order/order_bloc.dart';
-import 'package:flutter_fic7_app/bloc/Products/products_bloc.dart';
 import 'package:flutter_fic7_app/bloc/login/login_bloc.dart';
 import 'package:flutter_fic7_app/bloc/logout/logout_bloc.dart';
 import 'package:flutter_fic7_app/data/datasources/auth_local_datasource.dart';
